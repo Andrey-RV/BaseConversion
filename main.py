5b1373a1d61016c2e449bf677325fdf95155755a
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-03-12 20:32:39 -0300

    Now includes example of converting from decimal

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from base_conversion import binary
 from base_conversion import hexadecimal
+from base_conversion import decimal
 
 
 def main():
-    binary_number = binary.Binary('1010101')
+    binary_number = binary.Binary(1)
     print(binary_number.to_decimal())
     print(binary_number.to_hex())
 
-    hex_number = hexadecimal.Hexadecimal('A2C')
+    hex_number = hexadecimal.Hexadecimal('1')
     print(hex_number.to_decimal())
     print(hex_number.to_binary())
+
+    decimal_number = decimal.Decimal(1)
+    print(decimal_number.to_binary())
+    print(decimal_number.to_hex())
+
 
 if __name__ == '__main__':
     main()
